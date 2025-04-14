2964fa393bc6775998822c6c0d834c9652334aa2
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-04-14 14:00:17 -0500

    Added Menu component and MenuView

---
+++ src/components/Menu.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+export default {
+    name: "Menu"
+}
+</script>
+<template>
+    <div class="menu_wrapper">
+        <h1>Menu</h1>
+
+        <!-- Menu -->
+        <div class="menu">
+            <h3>Pizza</h3>
+            <table>
+                <tbody>
+                    <tr>
+                        <td><strong>~Pepperoni~</strong></td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <small>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Magnam porro magni dicta
+                                debitis. Vel in reiciendis officiis dolorum quas, corporis vero ab voluptates laborum
+                                unde nulla, eum possimus, provident a?
+                            </small>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>9"</td>
+                        <td>$6.95</td>
+                        <td><button type="button" class="btn_green">+</button></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
---
+++ src/views/MenuView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Menu from '../components/Menu.vue'
+</script>
+
+<template>
+    <main>
+        <Menu></Menu>
+    </main>
+</template>
