098dea14d17b964650a61f123e4b34e49ed058f4
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-04-14 12:45:21 -0500

    Changed HomeView component, updated Home.vue.

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export default {
+    name: "appHome"
+}
+</script>
+<template>
+    <div class="background">
+        <div class="background-overlay">
+            <h2>Welcome to Pizza Planet!</h2>
+            <img src="../assets/images/roller.png" alt="Roller image">
+            <h3>Feeling Hungry?</h3>
+            <button class="order_btn">Let's Order!</button>
+        </div>
+    </div>
+</template>
