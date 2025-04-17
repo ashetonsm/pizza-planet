13b69af81ccda7f607d52d33ffc80a737953a100
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-04-16 22:21:10 -0500

    Added Admin to HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import Admin from '@/components/Admin.vue';
 import Home from '../components/Home.vue'
 </script>
 
 <template>
   <main>
     <Home></Home>
+    <Admin></Admin>
   </main>
 </template>
