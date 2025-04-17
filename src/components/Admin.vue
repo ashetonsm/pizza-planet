a0305effa0bae1a73abaaba44acffc1e175c7fce
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-04-16 22:21:00 -0500

    Created Admin.vue

---
+++ src/components/Admin.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+export default {
+    name: "Admin"
+}
+</script>
+<template>
+    <div class="admin_wrapper">
+        <div class="current_user_wrapper">
+            Lorem
+        </div>
+        <div class="menu_wrapper">
+            ipsum
+        </div>
+        <div class="orders_wrapper">
+            dolor
+        </div>
+    </div>
+</template>
