db045b54bc77c2dd45834e01ee0bc232b23acb1b
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-04-17 21:16:28 -0500

    Added order table and example entry.

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -31,7 +31,68 @@ export default {
             </table>
         </div>
         <div class="orders_wrapper">
-            dolor
+            <h3>Current Orders: (5)</h3>
+            <table>
+                <thead>
+                    <tr>
+                        <th>
+                            Item
+                        </th>
+                        <th>
+                            Size
+                        </th>
+                        <th>
+                            Quantity
+                        </th>
+                        <th>
+                            Price
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="order_number">
+                        <th colspan="4">
+                            <strong>Order Number: 4</strong>
+                            <button type="button" class="btn_red">&times;</button>
+                        </th>
+                    </tr>
+                    <tr>
+                        <td>Margherita</td>
+                        <td>9"</td>
+                        <td>2</td>
+                        <td>$13</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </template>
+
+<style scoped>
+.admin_wrapper {
+    margin: 10px;
+}
+
+.current_user_wrapper,
+.order_wrapper,
+.menu_wrapper {
+    margin: 10px 0;
+    padding: 10px;
+    border: solid 1px #f79e38;
+
+}
+
+table {
+    text-align: left;
+    width: 70vw;
+}
+
+.order_number th {
+    background: #ddd;
+}
+
+.order_number button {
+    margin: 0 10px;
+
+}
+</style>
