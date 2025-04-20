90a218f16c5680f62b46ff01e8333aec24fed183
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-04-19 21:21:35 -0500

    Basic login with error message display.

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,37 @@
 <script lang="ts">
+
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { firebaseAuth } from '../firebase';
+
+
+
 export default {
-    name: "Login"
+    name: "Login",
+    data(vm) {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        async signIn() {
+            signInWithEmailAndPassword(firebaseAuth, this.email, this.password)
+                .then((userCredential) => {
+                    //Signed in
+                    const user = userCredential.user;
+                    alert('Welcome, ' + user.email);
+
+                })
+                .catch((error) => {
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    alert('Error Code: ' + errorCode + '--- Error Message: ' + errorMessage);
+                })
+        }
+    }
 }
+
+
 </script>
 
 <template>
@@ -9,13 +39,13 @@ export default {
         <form>
             <div>
                 <label for="email">Email Address:</label>
-                <input type="email" id="email" placeholder="Enter email">
+                <input type="email" id="email" v-model="email" placeholder="Enter email">
             </div>
             <div>
                 <label for="password">Password:</label>
-                <input type="password" id="password" placeholder="Enter password">
+                <input type="password" id="password" v-model="password" placeholder="Enter password">
             </div>
-            <button type="button" class="btn_green">Sign In</button>
+            <button type="button" class="btn_green" @click.prevent="signIn()">Sign In</button>
 
 
         </form>
