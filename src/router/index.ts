4a021df04636a8d8672ec4e7c975bebb276ca8fc
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-04-20 22:16:57 -0500

    Added a 404 NotFound View and route.

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,10 +3,16 @@ import HomeView from '../views/HomeView.vue'
 import MenuView from '@/views/MenuView.vue'
 import AdminView from '@/views/AdminView.vue'
 import ContactView from '@/views/ContactView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/:pathMatch(.*)*', 
+      name: 'not-found',
+      component: NotFoundView
+    },
     {
       path: '/',
       name: 'home',
