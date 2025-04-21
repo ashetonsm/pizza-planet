9aa75a4a7d9b493be4a4e056fe6fa9fc6303d4e5
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-04-21 12:31:35 -0500

    Split routes from index.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,58 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import MenuView from '@/views/MenuView.vue'
-import AdminView from '@/views/AdminView.vue'
-import ContactView from '@/views/ContactView.vue'
-import NotFoundView from '@/views/NotFoundView.vue'
-import History from '@/components/History.vue'
-import Delivery from '@/components/Delivery.vue'
-import OrderGuide from '@/components/OrderGuide.vue'
+import { routes } from '@/router/routes'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/:pathMatch(.*)*', 
-      name: 'not-found',
-      component: NotFoundView
-    },
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'), children: [
-        {path: '/history', name: 'history', component: History},
-        {path: '/delivery', name: 'delivery', component: Delivery},
-        {path: '/orderGuide', name: 'orderGuide', component: OrderGuide},
-      ]
-    },
-    {
-      path: '/menu',
-      name: 'menu',
-      component: MenuView,
-    },
-    {
-      path: '/admin',
-      name: 'admin',
-      component: AdminView,
-      beforeEnter: (to, from, next) => {
-        alert('This area is for authorized users only. Please sign in to continue.')
-        next()
-      }
-    },
-    {
-      path: '/contact',
-      name: 'contact',
-      component: ContactView,
-    },
-  ], scrollBehavior(to, from, savedPosition) {
+  routes,
+ scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
       return savedPosition
     }
