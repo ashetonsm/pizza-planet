764d22edebdbc2e42b52ff61b8b0d43dd37b8cac
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-04-22 14:47:07 -0500

    Moved menuItems into a module for no reason

---
+++ src/stores/modules/menu.ts
@@ -0,0 +1,39 @@
+export const menuItems =
+    [{
+        name: 'Margherita',
+        description: 'A delicious tomato based pizza topped with mozzarella',
+        quantity: 1,
+        options: [{
+            size: 9,
+            price: 6.95
+        }, {
+            size: 12,
+            price: 10.95
+        }]
+    },
+    {
+        name: 'Pepperoni',
+        description: 'A delicious tomato based pizza topped with mozzarella and pepperoni',
+        quantity: 1,
+        options: [{
+            size: 9,
+            price: 7.95
+        }, {
+            size: 12,
+            price: 12.95
+        }]
+    },
+    {
+        name: 'Ham and Pineapple',
+        description: 'A delicious tomato based pizza topped with mozzarella, ham and pineapple',
+        quantity: 1,
+        options: [{
+            size: 9,
+            price: 7.95
+        }, {
+            size: 12,
+            price: 12.95
+        }]
+    }
+
+    ]
--- src/stores/store.ts
+++ src/stores/store.ts
@@ -2,48 +2,12 @@ import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import { signInWithEmailAndPassword, signOut, type User } from 'firebase/auth';
 import { firebaseAuth } from '../firebase';
+import { menuItems } from './modules/menu.ts';
+
 export const useMenuStore = defineStore('menuItems', {
     state: (): State => {
         return {
-            menuItems:
-                [{
-                    name: 'Margherita',
-                    description: 'A delicious tomato based pizza topped with mozzarella',
-                    quantity: 1,
-                    options: [{
-                        size: 9,
-                        price: 6.95
-                    }, {
-                        size: 12,
-                        price: 10.95
-                    }]
-                },
-                {
-                    name: 'Pepperoni',
-                    description: 'A delicious tomato based pizza topped with mozzarella and pepperoni',
-                    quantity: 1,
-                    options: [{
-                        size: 9,
-                        price: 7.95
-                    }, {
-                        size: 12,
-                        price: 12.95
-                    }]
-                },
-                {
-                    name: 'Ham and Pineapple',
-                    description: 'A delicious tomato based pizza topped with mozzarella, ham and pineapple',
-                    quantity: 1,
-                    options: [{
-                        size: 9,
-                        price: 7.95
-                    }, {
-                        size: 12,
-                        price: 12.95
-                    }]
-                }
-
-                ],
+            menuItems,
             orders: [],
             currentUser: null
         }
