73390874b98189417850583da24755103be42758
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-04-23 15:40:55 -0500

    Added a second script and created function to run the setMenuRef function.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
 import Header from './components/Header.vue';
+import { useMenuStore } from './stores/store';
+</script>
+
+<script lang="ts">
+export default {
+  name: 'app',
+  components: {
+    appHeader: Header
+  },
+  created() {
+    console.log('HELLO')
+    useMenuStore().setMenuRef()
+  }
+}
 </script>
 
 <template>
   <Header></Header>
 </template>
-
-<style scoped></style>
