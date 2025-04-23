41bf77d41c562e3819cd77e21e26a51e65cab3fa
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-04-23 14:24:25 -0500

    Removed counter test.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <script setup lang="ts">
 import Header from './components/Header.vue';
-import { useCounterStore } from './stores/counter';
-const store = useCounterStore();
 </script>
 
 <template>
-  <div>
-    <p>Count: {{ store.count }}</p>
-    <p>Double Count: {{ store.doubleCount }}</p>
-    <button @click="store.increment()">Increase Count</button>
-  </div>
   <Header></Header>
 </template>
 
