50e3cd582051ab9e1cc45d7b18d706d252605ed2
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-04-28 13:19:01 -0500

    Added VueFire, added global Header component, updated Firebase version.

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,24 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { firebaseApp } from './firebase'
 
 import App from './App.vue'
 import router from './router'
+import { VueFire, VueFireAuth } from 'vuefire'
+import Header from './components/Header.vue'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueFire, {
+    firebaseApp,
+    modules: [
+        VueFireAuth(),
+    ]
+
+})
+app.component('Header', Header)
 
 app.mount('#app')
