f31afdf2c072066adb413a51d870218647e3d97f
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-05-02 17:31:11 -0500

    Added a manual "load orders" button. Should be temporary.

--- src/components/AccountInformation.vue
+++ src/components/AccountInformation.vue
@@ -2,6 +2,7 @@
 import Login from './Login.vue';
 import Admin from './Admin.vue';
 import { useMenuStore } from '@/stores/store';
+import OrderHistory from '@/components/OrderHistory.vue';
 
 export default {
     setup() {
@@ -12,6 +13,10 @@ export default {
         signOut() {
             useMenuStore().signOut()
         },
+        loadOrders() {
+            console.log(`Setting orders`)
+            useMenuStore().setOrdersRef()
+        }
     }
 }
 </script>
@@ -20,6 +25,8 @@ export default {
         <section v-if="menuStore.currentUser !== null">
             <p>Logged in as: {{ menuStore.currentUser?.email }}</p>
             <button type="button" class="btn_red" @click="signOut()">Sign Out</button>
+            <button type="button" class="btn_green" @click="loadOrders()">Load Orders</button>
+            <OrderHistory></OrderHistory>
         </section>
         <section v-if="menuStore.admin === true">
             <Admin></Admin>
