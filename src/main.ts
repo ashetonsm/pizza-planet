ffc64b3fdc6eabcf1949a47b218ecdb789bd768d
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-05-02 17:30:49 -0500

    Added more components...

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ import Header from './components/Header.vue'
 import Admin from './components/Admin.vue'
 import Login from './components/Login.vue'
 import Register from './components/Register.vue'
+import OrderHistory from './components/OrderHistory.vue'
+import AccountInformation from '@/components/AccountInformation.vue';
 
 const app = createApp(App)
 
@@ -28,5 +30,7 @@ app.component('Header', Header)
 app.component('Admin', Admin)
 app.component('Login', Login)
 app.component('Register', Register)
+app.component('OrderHistory', OrderHistory)
+app.component('AccountInformation', AccountInformation)
 
 app.mount('#app')
