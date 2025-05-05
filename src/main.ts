3edd939bcad715aa355b03f3c4b099619e830e0b
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-05-05 18:41:58 -0500

    Adds the forbidden redirect if there's no current user.

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,20 @@ import { createPinia } from 'pinia'
 import { firebaseApp } from './firebase'
 
 import App from './App.vue'
-import router from './router'
-import { VueFire, VueFireAuth } from 'vuefire'
+import { getCurrentUser, VueFire, VueFireAuth } from 'vuefire'
+import { createRouter, createWebHistory } from 'vue-router'
+import { routes } from './router/routes'
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes
+})
+router.beforeEach(async (to, from) => {
+    const user = await getCurrentUser()
+    if (to.meta.isProtected && !user) {
+        return { name: 'forbidden' }
+    }
+})
 
 const app = createApp(App)
 
