1f0e17f42a8cf26a1778c0fc4ff91c5cb3a26293
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-05-04 20:06:43 -0500

    Displays orders in a more organized way (except for the duplicate remove buttons).

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
 import NewMenuItem from './NewMenuItem.vue';
 import { useMenuStore } from '@/stores/store';
-import type { Item, Order } from '@/stores/Item';
+import type { Item } from '@/stores/Item';
 
 export default {
     components: {
@@ -15,10 +15,11 @@ export default {
         removeItem(item: Item) {
             useMenuStore().removeItem(item)
         },
-        removeOrder(order: Order) {
-            if (confirm('Remove Order?')) {
-                useMenuStore().removeOrder(order)
-            }
+        removeOrder(order: any) {
+            console.log(order)
+            // if (confirm('Remove Order?')) {
+            useMenuStore().removeOrder(order)
+            // }
         }
     }
 }
@@ -54,18 +55,22 @@ export default {
                 <!-- Now an array of orders -->
                 <tbody v-for="(orderArray, index) in menuStore.orders" :key="index">
                     <tr class="order_number">
-                        <th colspan="4">
-                            <strong>Order Id: {{ index + 1 }}</strong>
-                            <!-- <button type="button" class="btn_red" @click="removeOrder(orderArray)">&times;</button> -->
-                        </th>
+                        <th>Remove Order</th>
+                        <th>User Id: {{ index + 1 }}</th>
+                        <th>Basket</th>
                     </tr>
                     <!-- Now an array of order objects -->
-                    <tr v-for="usersOrders in orderArray.orders as any" :key="usersOrders.id + index">
-                        <td v-for="item in usersOrders.basket.items">{{ item.name }}</td>
-                        <td v-for="item in usersOrders.basket.items">{{ item.size }}"</td>
-                        <td v-for="item in usersOrders.basket.items">${{ item.price }}</td>
-                        <td v-for="item in usersOrders.basket.items">Qty: {{ item.quantity }}</td>
-
+                    <tr v-for="(usersOrders, index) in orderArray.orders as any" :key="usersOrders.date + index">
+                        <th><button class="btn_red remove-order-button" @click="removeOrder(usersOrders)">Remove
+                                Order</button></th>
+                        <th>User ID</th>
+                    <tr v-for="(item, index) in usersOrders.basket.items"
+                        :key="item.name + item.size + item.quantity + index">
+                        {{ item.name }}
+                        <td>{{ item.size }}"</td>
+                        <td>${{ item.price }}</td>
+                        <td>Qty: {{ item.quantity }}</td>
+                    </tr>
                     </tr>
                 </tbody>
             </table>
