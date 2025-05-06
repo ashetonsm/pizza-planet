6e7562e65f454f9c69aa92c607182b49f2b11805
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-05-05 20:09:16 -0500

    Updates favicon.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,10 +6,24 @@ import History from '@/components/History.vue';
 </script>
 
 <template>
-  <main>
-    <Home></Home>
-    <OrderGuide></OrderGuide>
-    <Delivery></Delivery>
-    <History></History>
-  </main>
+  <v-app id="inspire">
+    <v-app-bar extended>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Application</v-app-bar-title>
+
+      <v-btn icon="mdi-dots-vertical">
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col v-for="n in 24" :key="n" cols="4">
+            <v-card height="200"></v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
