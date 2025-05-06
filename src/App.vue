878e4b2a857793a052abc7a4fc6b97bb33dbb988
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-05-05 23:01:59 -0500

    Updated with new navigation. Tried to add theme toggling (unfinished)

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,31 @@
 <script lang="ts">
 import Header from './components/Header.vue';
 import { useMenuStore } from './stores/store';
+import { ref } from 'vue'
 
 export default {
   name: 'app',
   setup() {
+    const theme = ref('light')
     console.log('HELLO FROM APP.VUE')
     useMenuStore().setMenuRef()
     useMenuStore().setAdminsRef()
   },
   components: {
     Header
-  }
+  },
+  data() {
+    return {
+      theme: ref('light'),
+    }
+  },
 }
 </script>
 
 <template>
-  <v-app id="inspire">
-    <Header></Header>
+  <v-app id="inspire" :theme="theme">
+    <v-container fluid>
+      <Header></Header>
+    </v-container>
   </v-app>
 </template>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,28 +1,58 @@
 <script lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
+import PPLogo from '../assets/images/PPLogo.png'
+import { ref } from 'vue'
 
 export default {
+    setup() {
+        const theme = ref('light')
+    },
     components: {
         RouterLink,
         RouterView
+    },
+    data() {
+        return {
+            theme: ref('light'),
+            items: [
+                { title: 'Home', name: 'home' },
+                { title: 'Menu', name: 'menu' },
+                { title: 'About', name: 'about' },
+                { title: 'Account', name: 'account' },
+            ]
+        }
+    },
+    methods: {
+        onClick() {
+            this.theme = this.theme === 'light' ? 'dark' : 'light'
+        }
     }
 };
 </script>
 
 <template>
     <v-app-bar>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <img class="header-logo" src="../assets/images/PPLogo.png" />
         <v-app-bar-title>Pizza Planet</v-app-bar-title>
-        <nav>
-            <RouterLink :to="{ name: 'home' }">Home</RouterLink>
-            <RouterLink :to="{ name: 'menu' }">Menu</RouterLink>
-            <RouterLink :to="{ name: 'contact' }">Contact</RouterLink>
-            <RouterLink :to="{ name: 'about' }">About</RouterLink>
-            <RouterLink :to="{ name: 'account' }">Account</RouterLink>
-        </nav>
+        <v-btn :prepend-icon="theme === 'light' ? 'mdi-weather-sunny' : 'mdi-weather-night'" text="Toggle Theme" slim
+            @click="onClick()"></v-btn>
+        <v-menu>
+            <template v-slot:activator="{ props }">
+                <v-btn icon="mdi-dots-vertical" variant="text" v-bind="props"></v-btn>
+            </template>
 
-        <v-btn icon="mdi-dots-vertical">
-        </v-btn>
+            <v-list>
+                <v-list-item v-for="(item, i) in items" :key="i" :value="i" @click="$router.push({ name: item.name })">
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+            </v-list>
+        </v-menu>
     </v-app-bar>
     <RouterView />
 </template>
+
+<style lang="css" scoped>
+.header-logo {
+    max-height: 3em;
+}
+</style>
