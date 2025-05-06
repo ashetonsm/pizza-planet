8f13faf005fa566c639b5e5651078f6b05fcdadf
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-05-05 20:21:57 -0500

    Header with no list element.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,30 +10,19 @@ export default {
 </script>
 
 <template>
-    <header>
-        <div class="header-background">
-            <img src="../assets/images/PPLogo.png" alt="Pizza Planet Logo">
-            <h1 class="site-title">Pizza Planet</h1>
-            <nav>
-                <ul>
-                    <li>
-                        <RouterLink :to="{ name: 'home' }">Home</RouterLink>
-                    </li>
-                    <li>
-                        <RouterLink :to="{ name: 'menu' }">Menu</RouterLink>
-                    </li>
-                    <li>
-                        <RouterLink :to="{ name: 'contact' }">Contact</RouterLink>
-                    </li>
-                    <li>
-                        <RouterLink :to="{ name: 'about' }">About</RouterLink>
-                    </li>
-                    <li>
-                        <RouterLink :to="{ name: 'account' }">Account</RouterLink>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+    <v-app-bar>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-app-bar-title>Pizza Planet</v-app-bar-title>
+        <nav>
+            <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+            <RouterLink :to="{ name: 'menu' }">Menu</RouterLink>
+            <RouterLink :to="{ name: 'contact' }">Contact</RouterLink>
+            <RouterLink :to="{ name: 'about' }">About</RouterLink>
+            <RouterLink :to="{ name: 'account' }">Account</RouterLink>
+        </nav>
+
+        <v-btn icon="mdi-dots-vertical">
+        </v-btn>
+    </v-app-bar>
     <RouterView />
 </template>
