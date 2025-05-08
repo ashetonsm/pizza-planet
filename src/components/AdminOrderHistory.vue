672aa2590fe5ecc3ea0df94414e6295689631f15
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-05-08 13:24:39 -0500

    Split from Admin

---
+++ src/components/AdminOrderHistory.vue
@@ -0,0 +1,100 @@
+<script lang="ts">
+import { useMenuStore } from '@/stores/store';
+import type { Item } from '@/stores/Item';
+
+export default {
+    data() {
+        return {
+            displayDetails: false,
+            selectedOrder: {
+                id: -1,
+                status: -1,
+                items: []
+            }
+        }
+    },
+    setup() {
+        const menuStore = useMenuStore()
+        return { menuStore }
+    },
+    methods: {
+        removeItem(item: Item) {
+            useMenuStore().removeItem(item)
+        },
+        handleClose() {
+            if (this.displayDetails === false) {
+                this.displayDetails = true;
+            } else {
+
+                this.displayDetails = false
+                this.selectedOrder = {
+                    id: -1,
+                    status: -1,
+                    items: []
+                }
+            }
+        },
+        removeOrder(order: any) {
+            console.log(order)
+            // if (confirm('Remove Order?')) {
+            useMenuStore().removeOrder(order)
+            // }
+        },
+        viewOrder(orderIndex: any, orderData: any) {
+            console.log(orderIndex)
+            this.selectedOrder = {
+                id: orderIndex,
+                status: orderData.orderStatus,
+                items: orderData.basket.items
+            }
+            this.handleClose()
+        },
+    }
+}
+</script>
+<template>
+    <!-- MAKE INTO A MODAL -->
+    <section v-if="displayDetails === true">
+        <button class="btn_red" @click="handleClose()">CLOSE</button>
+        <h3>Order #{{ selectedOrder.id }}:</h3>
+        <h2>Status: {{ selectedOrder.status }}</h2>
+        <ul v-for="i in selectedOrder.items as any">
+            <li>{{ i.name }}</li>
+            <li>{{ i.size }}</li>
+            <li>${{ i.price }}</li>
+            <li>Qty: {{ i.quantity }}</li>
+        </ul>
+    </section>
+
+    <v-card class="mx-auto px-6" title="Admin Order Management">
+        <v-card-item class="align-items-center justify-center">
+            <table>
+                <!-- Now an array of orders -->
+                <tbody v-for="(orderArray, index) in menuStore.orders" :key="index">
+                    <tr class="order_number">
+                        <th>Remove Order</th>
+                        <th>Email</th>
+                        <th>Basket</th>
+                        <th>Order Status</th>
+                        <th>Details</th>
+                    </tr>
+                    <!-- Now an array of order objects -->
+                    <tr v-for="(usersOrders, index) in orderArray.orders as any" :key="usersOrders.date + index">
+                        <th><button class="btn_red remove-order-button" @click="removeOrder(usersOrders)">Remove
+                                Order</button></th>
+                        <th>{{ usersOrders.userEmail }}</th>
+                    <tr v-for="(item, index) in usersOrders.basket.items"
+                        :key="item.name + item.size + item.quantity + index">
+                        {{ item.name }}
+                        <td>{{ item.size }}"</td>
+                        <td>${{ item.price }}</td>
+                        <td>Qty: {{ item.quantity }}</td>
+                    </tr>
+                    <th>{{ usersOrders.orderStatus }}</th>
+                    <th><button class="btn_green" @click="viewOrder(index, usersOrders)">View</button></th>
+                    </tr>
+                </tbody>
+            </table>
+        </v-card-item>
+    </v-card>
+</template>
