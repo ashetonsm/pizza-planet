fae61651477505ab35c7657e637952eb7814d4ed
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-05-07 20:16:37 -0500

    Removed line breaks.

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -28,11 +28,9 @@ export default {
 <template>
     <v-sheet class="mx-auto" max-width="300">
         <v-card class="mx-auto px-6 py-8" max-width="344" title="Register">
-
             <v-form validate-on="submit" @submit.prevent="submit">
                 <v-text-field v-model=email label="Email:" type="email"></v-text-field>
                 <v-text-field v-model=password label="Password:" type="password"></v-text-field>
-
                 <v-btn class="mt-2" text="Submit" type="submit" block></v-btn>
             </v-form>
         </v-card>
