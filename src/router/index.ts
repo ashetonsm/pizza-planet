0d5ab6d3582be74ed687ef68b3aa08f0c1d7d4ed
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-05-13 20:46:34 -0500

    Deleted unused import.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from '@/router/routes'
-import { getCurrentUser } from 'vuefire'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
