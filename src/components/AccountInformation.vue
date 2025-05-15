dd99b65d3d0bbc641eb7de9246fc5776a8953d27
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-05-15 16:15:10 -0500

    Moved v-ifs into AdminInformation

--- src/components/AccountInformation.vue
+++ src/components/AccountInformation.vue
@@ -1,17 +1,13 @@
 <script lang="ts">
 import { useMenuStore } from '@/stores/store';
-import Admin from './Admin.vue';
-import Login from './Login.vue';
 import OrderHistory from './OrderHistory.vue';
-import AdminOrderHistory from './AdminOrderHistory.vue';
 import { firebaseAuth } from '@/firebase';
+import AdminInformation from './AdminInformation.vue';
 
 export default {
     components: {
-        Admin,
-        Login,
         OrderHistory,
-        AdminOrderHistory
+        AdminInformation
     },
     setup() {
         const menuStore = useMenuStore()
@@ -29,48 +25,27 @@ export default {
     },
     methods: {
         signOut() {
-            this.menuStore.signOut()
+            this.menuStore.signOut();
+            this.$router.push({name: 'login'});
         }
     }
 }
 </script>
 <template>
     <v-container>
-
-        <Login v-if="auth.currentUser === null"></Login>
-
-        <v-container class="d-flex" v-else-if="auth.currentUser !== null">
+        
+        <v-container class="d-flex">
             <v-card class="d-flex mx-auto px-6" :title=auth.currentUser!.email?.toString()>
                 <v-divider vertical></v-divider>
                 <v-btn class="ml-5 my-auto" variant="tonal" @click="signOut()">Sign Out</v-btn>
             </v-card>
         </v-container>
 
-        <v-container v-if="menuStore.admin === true">
-            <v-expansion-panels>
-                <v-expansion-panel>
-                    <v-expansion-panel-title>
-                        <span class="text-h5">Menu Management</span>
-                    </v-expansion-panel-title>
-                    <v-expansion-panel-text>
-                        <Admin />
-                    </v-expansion-panel-text>
-                </v-expansion-panel>
-                <v-expansion-panel>
-                    <v-expansion-panel-title>
-                        <span class="text-h5">Order Management</span>
-                    </v-expansion-panel-title>
-                    <v-expansion-panel-text>
-                        <AdminOrderHistory />
-                    </v-expansion-panel-text>
-                </v-expansion-panel>
-            </v-expansion-panels>
-        </v-container>
-
-        <v-container class="d-flex" v-if="menuStore.admin === false && auth.currentUser !== null">
-            <v-card class="mx-auto px-6" title="Order History">
+        <v-container class="d-flex">
+            <v-card class="mx-auto px-6 text-center" title="Order History" v-if="!menuStore.admin">
                 <OrderHistory></OrderHistory>
             </v-card>
+                <AdminInformation v-else="menuStore.admin"></AdminInformation>
         </v-container>
 
     </v-container>
